8da5a8c389621a66eba1a0fb3ac00885f5e4aba1
ccrismota <ccrismota@gmail.com>
2023-03-26 09:09:34 -0300

    Componentes(feat): Criação dos componentes - pagina principal, rodapé(Footer), recuperar senha, carrinho, login, cadastro, landingpage, navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 
+const routes: Routes = [
+  { path: '', component: PaginaPrincipalComponent ,
+
+  children: [
+    {path: '', component: LandingPageComponent},
+
+
+]},
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
+import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    NavbarComponent,
+    CarrinhoComponent,
+    RecuperarSenhaComponent,
+    LoginComponent,
+    CadastroComponent,
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
